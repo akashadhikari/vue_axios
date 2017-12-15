7b525cb22217eb1f7fbeb614b18a939a0c5acb66
Akash Adhikari <akash_532998@yahoo.com>
2017-12-15 15:51:26 +0545

    the api key

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-// API KEY: b2ed2bd00e3742b0b7358a14358a5efc
+// ADD YOUR API KEY
 
 const vm = new Vue({
   el: '#app',
   data: {
-    results: [
-      {title: "the very first post", abstract: "post 1"},
-      {title: "and then there was the second", abstract: "post 2"},
-      {title: "third time's a charm", abstract: "post 3"},
-      {title: "four the last time", abstract: "post 4"}
-    ]
+    results: []
+  }, mounted() {
+
+  	axios.get("https://api.nytimes.com/svc/topstories/v2/home.json?api-key=add_your_api_key")
+    .then(response => {this.results = response.data.results})
+
   }
 });
