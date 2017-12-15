ab9df607510d0333c0aa5cd43d8805d355730ba7
Akash Adhikari <akash_532998@yahoo.com>
2017-12-15 15:41:13 +0545

    setting up first

---
+++ app.js
@@ -0,0 +1,13 @@
+// API KEY: b2ed2bd00e3742b0b7358a14358a5efc
+
+const vm = new Vue({
+  el: '#app',
+  data: {
+    results: [
+      {title: "the very first post", abstract: "post 1"},
+      {title: "and then there was the second", abstract: "post 2"},
+      {title: "third time's a charm", abstract: "post 3"},
+      {title: "four the last time", abstract: "post 4"}
+    ]
+  }
+});
